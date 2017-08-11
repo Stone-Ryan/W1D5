080d318d48c45792be3fba51e4cde021d602f404
App Academy Student <student@appacademy.io>
2017-08-11 15:35:55 -0700

    Finished hashing functions for Stings arrays and hashes

--- lru/lib/p02_hashing.rb
+++ lru/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hash_val = 0
+    each_with_index { |ele, i| hash_val += (ele.to_s.ord * i) }
+    hash_val
   end
 end
 
 class String
   def hash
+    hash_val = 0
+    chars.each { |char| hash_val += (char.ord + self.index(char)) }
+    hash_val
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort.hash
   end
 end
